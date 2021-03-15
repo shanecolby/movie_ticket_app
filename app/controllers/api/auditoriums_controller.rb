31e7793765c1214f3d725c6d055dfc46cda43dd5
shane <shanecolbyzero8@gmail.com>
2021-03-15 18:19:10 -0500

    add actions and views for auditoriums

--- app/controllers/api/auditoriums_controller.rb
+++ app/controllers/api/auditoriums_controller.rb
@@ -5,11 +5,26 @@ class Api::AuditoriumsController < ApplicationController
   end
 
   def show
+    @auditorium = Auditorium.find_by(id: params[:id])
+    render "show.json.jb"
   end
 
   def update
+    @auditorium = Auditorium.find_by(id: params[:id])
+
+    @auditorium.theater_id = params[:theater_id] || @auditorium.theater_id,
+    @auditorium.auditorium_number = params[:auditorium_number] || @auditorium.auditorium_number,
+    @auditorium.seating_capacity = params[:seating_capacity] || @auditorium.seating_capacity
+
+    # @auditorium.save
+    render "show.json.jb"
   end
 
   def destroy
+    @auditorium = Auditorium.find_by(id: params[:id])
+    @auditorium.destroy
+    render json: {
+      message: "auditorium has been removed"
+    }
   end
 end
