be78a7ce28aba78017c984ddc7de80d3d650d07d
shane <shanecolbyzero8@gmail.com>
2021-03-15 16:53:37 -0500

    add movies routes index and show views along with all actions

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,40 @@
 class Api::MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+    render "index.json.jb"
+
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @movie = Movie.new(
+      title: params[:title]
+    )
+    # @movie.save!
+    render "show.json.jb"
+
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+
+    @movie.title = params[:title] || @movie.title
+
+    # @movie.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    render json: {
+      message: "Movie has been removed"
+    }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    # get "/movies" => "movies#index"
+    root to: "movies#index"
+    resources :movies
+  end
 end
